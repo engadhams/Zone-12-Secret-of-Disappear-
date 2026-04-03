cfa840c430859164b431c90b77458d60f3a42fb9
Mostafa750-dev <mostafaekramy57560@gmail.com>
2026-04-03 23:05:11 +0200

    1

---
+++ Zone 12 (SoD)/Playermovment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 500f; // سرعة التحريك
+
+    float horizontalInput;
+    float verticalInput;
+
+    void Update()
+    {
+        // استقبال المدخلات من اللاعب (Update)
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // تطبيق الحركة الفيزيائية (FixedUpdate)
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        
+        // استخدام AddForce لإضافة قوة دفع للجسم
+        rb.AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+}
