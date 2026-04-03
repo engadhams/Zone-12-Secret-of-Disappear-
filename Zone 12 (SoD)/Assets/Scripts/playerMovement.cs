4e9d31c5a394bdc06cb727d0507153a22170e5d8
Adham Said <adsaid2022@gmail.com>
2026-04-03 23:39:20 +0200

    1

--- Zone 12 (SoD)/Assets/Scripts/playerMovement.cs
+++ Zone 12 (SoD)/Assets/Scripts/playerMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 5;
+    public float mouseSens = 2;
     public float horiz;
     public float vert;
+    public float mouseX;
+    public float mouseY;
     public GameObject player;
     public Rigidbody rb;
 
@@ -19,11 +22,24 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //This is a comment
-       horiz = Input.GetAxis("Horizontal");
-       vert = Input.GetAxis("Vertical");
-       Vector3 move = vert* transform.forward + transform.right *horiz;
+        Move();
+        Look();
+    }
+    void Move()
+    {
+        horiz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+        Vector3 move = (vert* transform.forward + transform.right *horiz) *moveSpeed;
+        rb.velocity = new Vector3(move.x ,rb.velocity.y, move.z);
+    }
 
-       rb.velocity = new Vector3(move.x ,rb.velocity.y, move.z);
+    void Look()
+    {
+        mouseX = Input.GetAxis("Mouse Y")*mouseSens;
+        mouseY = Input.GetAxis("Mouse X")*mouseSens;
+
+        Quaternion rotValue = Quaternion.Euler(mouseX,mouseY,0);
+        
+        transform.rotation = rotValue;
     }
 }
