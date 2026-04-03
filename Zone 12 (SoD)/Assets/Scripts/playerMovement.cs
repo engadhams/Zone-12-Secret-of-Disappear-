5ab362ad4ad473cf56df01172739ec48ac6d1739
Adham Said <adsaid2022@gmail.com>
2026-04-03 21:35:36 +0200

    1

--- Zone 12 (SoD)/Assets/Scripts/playerMovement.cs
+++ Zone 12 (SoD)/Assets/Scripts/playerMovement.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    float horiz;
-    float vert;
+    public float horiz;
+    public float vert;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,8 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       horiz = Input.GetAxis("Horizontal");
+       vert = Input.GetAxis("Vertical");
+
     }
 }
