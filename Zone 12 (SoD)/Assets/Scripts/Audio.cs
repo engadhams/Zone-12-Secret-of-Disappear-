ca93e635e88473fabdb3b496c2518ddde730a13b
Adham Said <adsaid2022@gmail.com>
2026-04-06 23:28:45 +0200

    1

---
+++ Zone 12 (SoD)/Assets/Scripts/Audio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    playerMovement plMoveScr;
+    AudioSource audSrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set Variables
+        plMoveScr = GetComponent<playerMovement>();
+        audSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    void FootstepSound()
+    {
+        
+    }
+
+}
