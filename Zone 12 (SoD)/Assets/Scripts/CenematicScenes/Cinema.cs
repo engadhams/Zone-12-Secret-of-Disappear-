9129cc9a2dc15a503a1bd4b72202832108ce9998
Adham Said <adsaid2022@gmail.com>
2026-06-30 11:33:16 +0200

    f

--- Zone 12 (SoD)/Assets/Scripts/CenematicScenes/Cinema.cs
+++ Zone 12 (SoD)/Assets/Scripts/CenematicScenes/Cinema.cs
@@ -4,71 +4,14 @@ using UnityEngine;
 
 public class Cinema : MonoBehaviour
 {
-    public GameObject player;
-    public Transform Desk;
-
-    [Header("Scene Cameras")]
-    public Camera mainCam;
-    public Camera cenCam;
-
-    bool playerMoveForward =false;
-    bool inCutScene=false;
-
-    playerMovement pM;
-
+    public GameObject cenCam;
     void Start()
     {
-        mainCam.enabled = true;
-        cenCam.enabled = false;
-        pM = player.GetComponent<playerMovement>();
+        Scene1();
     }
 
-    public void ActivateScene1()
+    void Scene1()
     {
-        // "GetMission" Scene
-        pM.isAbletoLook = false;
-        pM.isAbletoMove = false;
-        pM.anim.SetFloat("Speed", 1);
-
-        Vector3 dskPos =  new Vector3(Desk.position.x, player.transform.position.y, Desk.position.z);
-        player.transform.LookAt(dskPos);
-        pM.anim.SetFloat("Greet", 0);
-        inCutScene = true;
-        CameraSwitch();
-        playerMoveForward = true;
+        
     }
-
-    void CameraSwitch()
-    {
-        if(inCutScene)
-        {
-            mainCam.enabled = false;
-            cenCam.enabled = true;
-        }
-        else if(!inCutScene)
-        {
-            mainCam.enabled = true;
-            cenCam.enabled = false;
-        }
-    }
-    void Update()
-    {
-
-        // playerMoveForward Code
-        float distance = Vector3.Distance(player.transform.position, Desk.position);
-
-        if(playerMoveForward && distance > 2)
-        {
-            pM.isMoving = true;
-            pM.anim.SetFloat("Speed", 1);
-            player.transform.position = player.transform.position + player.transform.forward * 0.04f;
-        }
-        else if(playerMoveForward && distance < 2)
-        {
-            pM.isMoving = false;
-            pM.anim.SetFloat("Speed", 0);
-        }
-        //End
-    }
-
 }
